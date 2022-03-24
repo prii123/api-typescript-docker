16127be72be8569dc8441e9562b4cacc760fa2f4
prii123 <83259348+prii123@users.noreply.github.com>
2022-03-24 14:40:54 -0500

    crear nueva rama

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import config from 'dotenv';
 config.config();
 
 export = {
-  MONGODB_URI: "mongodb://147.182.239.233/empresas", //147.182.239.233
+  MONGODB_URI: "mongodb://localhost/empresas", //147.182.239.233
   PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "somesecretkey",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1y",
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -23,13 +23,16 @@ export const login = async (req: Request, res: Response) => {
         const user = await User.findOne({ email }).select("+password");
         // console.log(user)
         // if (!user) throw new Error("Invalid Username");
-        if (!user) return "Invalid";
-    
+        if (!user) {
+            return res.status(200).json('Invalid');
+        } //
+        // if (!user) return "Invalid";
+
         const validPassword = await comparePassword(password, user.password);
         // console.log(validPassword)
         // if (!validPassword) throw new Error("Invalid Password");
-        if (!validPassword) return "Invalid";
-    
+        if (!validPassword) return res.status(200).json("Invalid");
+
         const token = createJWTToken({
           _id: user._id,
           email: user.email,
@@ -39,5 +42,5 @@ export const login = async (req: Request, res: Response) => {
         const data:any = token + " " + user.email;
         console.log(data)
 
-        res.status(200).json(data);
+        return res.status(200).json(data);
 }
