425ec529df596e63c79aaccb63874aa8ebc2786a
prii123 <83259348+prii123@users.noreply.github.com>
2022-04-03 11:38:05 -0500

    se cambio la url a atlas por que la base de datos de digital osean se borro

--- config.ts
+++ config.ts
@@ -2,8 +2,9 @@ import config from 'dotenv';
 config.config();
 
 export = {
-  MONGODB_URI: "mongodb+srv://prii123:prii123@cluster0.ck6sw.mongodb.net/empresas?retryWrites=true&w=majority", //147.182.239.233 http://147.182.239.233/
+  MONGODB_URI: "mongodb+srv://prii123:yMhFMEVsIOYbdgkK@cluster0.ck6sw.mongodb.net/empresas?retryWrites=true&w=majority", //147.182.239.233 http://147.182.239.233/
   PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "somesecretkey",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1y",
 };
+
