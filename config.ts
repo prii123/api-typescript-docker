a0cda9fda754da28154f308481ee0224500a0b14
prii123 <83259348+prii123@users.noreply.github.com>
2022-05-30 15:14:29 -0500

    deploy

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import config from 'dotenv';
 config.config();
 
 export = {
-  MONGODB_URI: "mongodb+srv://prii123:yMhFMEVsIOYbdgkK@cluster0.ck6sw.mongodb.net/empresas?retryWrites=true&w=majority", //147.182.239.233 http://147.182.239.233/
+  MONGODB_URI: process.env.URL2,
   PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "somesecretkey",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1y",
