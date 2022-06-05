46bb2926952050f1b56e919274260b2079dea669
prii123 <83259348+prii123@users.noreply.github.com>
2022-06-05 18:35:17 -0500

    dploy

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import config from 'dotenv';
 config.config();
 
 export = {
-  MONGODB_URI: process.env.URL2,
+  MONGODB_URI: process.env.URL || "http:/localhost/",
   PORT: process.env.PORT || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "somesecretkey",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1y",
