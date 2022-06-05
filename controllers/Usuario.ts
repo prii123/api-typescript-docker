62140be1acf65dec17d381c45a07189436653f8d
prii123 <83259348+prii123@users.noreply.github.com>
2022-06-05 18:18:09 -0500

    arreglo de login

--- controllers/Usuario.ts
+++ controllers/Usuario.ts
@@ -10,6 +10,14 @@ class SubCuentasRouter {
     this.routes();
   }
 
+  async usuario(req: Request, res: Response): Promise<void> {
+    const id = req.verifiedUser._id;
+    const query = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    console.log(query)
+    res.json(query);
+  }
 
   async actualizar(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
@@ -19,12 +27,18 @@ class SubCuentasRouter {
     res.json(auxiliar);
   }
 
+
   routes() {
 
     this.router.put(
       "/actualizarusuario/:id",
       [authenticate],
       this.actualizar
+    );
+    this.router.get(
+      "/user",
+      [authenticate],
+      this.usuario
     );
   }
 }
